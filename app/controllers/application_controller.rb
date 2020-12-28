bba4fea239d265cb6fffdf6233f09000ff2398de
EC2 Default User <ec2-user@ip-172-31-74-89.ec2.internal>
2020-12-28 23:28:22 +0000

    add 管理者ログイン後のページ移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  #管理者がログイン後に、商品一覧へ飛ぶ設定。
+  #顧客がログイン後は、マイページに飛ぶよう設定の必要あり
+  def after_sign_in_path_for(resource)
+    admin_items_path(resource)
+  end
+
 end
